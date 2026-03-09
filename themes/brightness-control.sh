3ce0871bd5b6a0a457603d0ab2a610a5a9c4fff3
frmhd <freemiumhead@gmail.com>
2026-03-09 15:12:47 +0400

    change settings for smaller screen

--- themes/brightness-control.sh
+++ themes/brightness-control.sh
@@ -3,19 +3,21 @@
 # Usage: brightness-control.sh [up|down]
 
 if [ "$1" == "up" ]; then
-    ddcutil setvcp 10 + 10
+    brightnessctl set "10%+"
 elif [ "$1" == "down" ]; then
-    ddcutil setvcp 10 - 10
+    brightnessctl set "10%-"
 else
     echo "Usage: $0 [up|down]"
     exit 1
 fi
 
 # Get current brightness value
-brightness=$(ddcutil getvcp 10 | grep -oP 'current value =\s+\K\d+')
+brightness=$(brightnessctl get)
+
+percentage=$((brightness/655))
 
 # Display progress bar with swayosd
 swayosd-client \
     --custom-icon=display-brightness-symbolic \
-    --custom-progress=$(echo "scale=2; $brightness / 100" | bc) \
-    --custom-progress-text="$brightness%"
+    --custom-progress=$(echo "scale=2; $brightness / 65535" | bc) \
+    --custom-progress-text="$percentage%"
